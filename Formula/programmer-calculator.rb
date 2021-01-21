e14644a33d0e1a2bace2ce624a263290f57cc134
alt-romes <rodrigo.m.mesquita@gmail.com>
2021-01-21 18:55:45 +0000

    Write brew Formula

--- Formula/programmer-calculator.rb
+++ Formula/programmer-calculator.rb
@@ -1,37 +1,21 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ProgrammerCalculator < Formula
-  desc "command line programmer calculator"
+  desc "CLI Programming Calculator"
   homepage ""
   url "https://github.com/alt-romes/programmer-calculator/archive/v1.0.tar.gz"
-  sha256 "d2721bd354bc44698ca9c70f0d0f741623984addb6348a9c3bf74a1c1dcaf944"
-  license ""
+  sha256 "399524be160bf23251a170c949262204d650561b1e58c47af239284b3e3ae1da"
+  license "GNU General Public License v3.0"
 
   depends_on "cmake" => :build
   depends_on "ncurses"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    system "make", "default"
+    system "make", "install"
     bin.install "pcalc"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test programmer-calculator`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "true"
   end
 end
