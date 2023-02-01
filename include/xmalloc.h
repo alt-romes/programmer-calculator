e0cbf1c0d7f22963138dc77fb926c7ffae337d21
Manuel Bertele <manuel.bertele2003@gmail.com>
2023-02-01 21:18:53 +0100

    Fixed memory leak when alloc fails
    Temporary pointers were not freed when next alloc fails

--- include/xmalloc.h
+++ include/xmalloc.h
@@ -3,9 +3,15 @@
 
 #include <stdlib.h>
 
+#include "global.h"
+
 void *xmalloc(size_t bytes);
+void *xmalloc_with_ressources(size_t bytes, void** ressources, size_t nres);
 void *xcalloc(size_t nelem, size_t bytes);
+void *xcalloc_with_ressources(size_t nelem, size_t bytes, void** ressources, size_t nres);
 void *xrealloc(void *pntr, size_t bytes);
+void *xrealloc_with_ressources(void *pntr, size_t bytes, void** ressources, size_t nres);
+void xfreen(void** pntrs, size_t npntrs);
 void xfree(void *pntr);
 
 #endif
--- src/xmalloc.c
+++ src/xmalloc.c
@@ -4,26 +4,51 @@
 
 void *xmalloc(size_t bytes) {
 
-    void *temp = malloc(bytes);
-    if (temp == NULL)
+    return xmalloc_with_ressources(bytes, NULL, 0);
+}
+
+void *xmalloc_with_ressources(size_t bytes, void** ressources, size_t nres) {
+    void* temp = malloc(bytes);
+    if (temp == NULL) {
+        xfreen(ressources, nres);
         exit_pcalc(MEM_FAIL);
-    return (temp);
+    }
+    return temp;
 }
 
 void *xcalloc(size_t nelem, size_t bytes) {
 
+    return xcalloc_with_ressources(nelem, bytes, NULL, 0);
+}
+
+void *xcalloc_with_ressources(size_t nelem, size_t bytes, void** ressources, size_t nres) {
     void *temp = calloc(nelem, bytes);
-    if (temp == NULL)
+    if (temp == NULL) {
+        xfreen(ressources, nres);
         exit_pcalc(MEM_FAIL);
-    return (temp);
+    }
+    return temp;
 }
 
 void *xrealloc(void *pntr, size_t bytes) {
 
-    void *temp = pntr ? realloc(pntr, bytes) : malloc(bytes);
-    if (temp == NULL)
+    return xrealloc_with_ressources(pntr, bytes, NULL, 0);
+}
+
+void *xrealloc_with_ressources(void *pntr, size_t bytes, void** ressources, size_t nres) {
+
+    void *temp = pntr ? realloc(pntr, bytes) : xmalloc(bytes);
+    if (temp == NULL) {
+        xfreen(ressources, nres);
         exit_pcalc(MEM_FAIL);
+    }
     return (temp);
+}
+
+void xfreen(void** pntrs, size_t npntrs) {
+    for (size_t i = 0; i < npntrs; ++i) {
+        xfree(pntrs[i]);
+    }
 }
 
 void xfree(void *pntr) {
